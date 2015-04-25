82c09dfd4767c222f299cb4a17d10f0ce0843406
Brett Shollenberger <brett.shollenberger@gmail.com>
2015-04-24 23:54:16 -0700

    Add users client

--- gen-rb/client.rb
+++ gen-rb/client.rb
@@ -3,19 +3,78 @@ require "pry"
 $:.unshift File.dirname(__FILE__)
 Dir[File.expand_path(File.join(__FILE__, "../**/*"))].each { |f| require f }
 
-begin
-	port = ARGV[0] || 8000
+class ThriftClient
+  attr_accessor :port
 
-  transport = Thrift::FramedTransport.new(Thrift::Socket.new("127.0.0.1"))
-	protocol = Thrift::BinaryProtocol.new(transport)
-	client = UsersService::Client.new(protocol)
+  def initialize(options={})
+    @port = options.fetch(:port, 8000)
+    transport.open
+  end
 
-	transport.open()
+  def transport
+    @transport ||= Thrift::FramedTransport.new(Thrift::Socket.new("127.0.0.1"))
+  end
 
-	result = client.find()
-	puts result.inspect
-	
-	transport.close()
-rescue
-	puts $!
+  def protocol
+    @protocol ||= Thrift::BinaryProtocol.new(transport)
+  end
+
+  def client
+    @client ||= UsersService::Client.new(protocol)
+  end
+
+  def find(*args)
+    client.find(*args)
+  end
+
+  def create(*args)
+    client.create(*args)
+  end
+
+  def close
+    transport.close
+  end
+end
+
+class UsersModel
+  attr_accessor :client, :first_name, :last_name, :email, :id
+
+  class << self
+    attr_accessor :client
+
+    def find(*args)
+      UsersModel.new(from_thrift @client.find(*args))
+    end
+
+    def create(options={})
+      UsersModel.new(from_thrift @client.create(new_thrift(options)))
+    end
+
+    def new_thrift(options={})
+      thrift_user = User.new
+      options.each do |k, v|
+        thrift_user.send("#{k}=", v)
+      end
+
+      thrift_user
+    end
+
+    def from_thrift(user)
+      h = {}
+      user.struct_fields.values.each do |field|
+        h[field[:name].to_sym] = user.send(field[:name])
+      end
+
+      h
+    end
+  end
+
+  @client = ThriftClient.new
+
+  def initialize(options={})
+    @id         = options.fetch(:id)
+    @first_name = options.fetch(:first_name)
+    @last_name  = options.fetch(:last_name)
+    @email      = options.fetch(:email)
+  end
 end
--- users.thrift
+++ users.thrift
@@ -1,12 +1,13 @@
 include "types.thrift"
 
 struct User {
-  1: required i64 id,
+  1: optional i64 id,
   2: required string first_name,
   3: required string last_name,
   4: required string email,
 }
 
 service UsersService {
-  User find() throws (1:types.ServiceException e)
+  User find(1:i64 id) throws (1:types.ServiceException e)
+  User create(1:User user) throws (1:types.ServiceException e)
 }
